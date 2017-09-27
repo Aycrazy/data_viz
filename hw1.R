2d8b2d478d516f2c452ba4e0e95da7877b683a40
aycrazy <andrewyaspan@gmail.com>
2017-09-27 14:12:32 -0500

    install and load

---
+++ hw1.R
@@ -0,0 +1,20 @@
+ #Andrew Yaspan's exploratory analysis of DoT FARS data
+
+setwd("~/Documents/data_viz")
+
+install.packages('readr')
+install.packages('haven')
+install.packages('tidyr')
+install.packages('stringr')
+install.packages('ggplot2')
+install.packages('dplyr')
+
+library('readr')
+library('haven')
+library('tidyr')
+library('stringr')
+library('ggplot2')
+library('dplyr')
+
+acc2014 <- read_sas('accident.sas7bdat')
+acc2015 <- read_csv('nhtsa_2015_dir/accident.csv')
